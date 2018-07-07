4f037c76b09da5a86c0bc7c88cae790f5ee99b8b
ksscott <scott.ksteven@gmail.com>
2018-07-06 19:00:32 -0600

    Allow ChordSpec to specify "minor seventh" etc.

--- MusicComposition/src/theory/ChordSpec.java
+++ MusicComposition/src/theory/ChordSpec.java
@@ -1,14 +1,21 @@
 package theory;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static theory.ChordSpec.Degree.*;
+
 public class ChordSpec {
 	
 	private Note tonic;
 	private Quality quality;
 	private Degree degree;
-	private boolean add2; // overlap with degree, change implementation?
-	private boolean add4;
-	private boolean add6;
+//	private boolean add2; // overlap with degree, change implementation?
+//	private boolean add4;
+//	private boolean add6;
 	private int inversion;
+	
+	private Map<Degree,Quality> degreeQualityMap;
 	
 	public ChordSpec(Note tonic) {
 		this(tonic, Quality.MAJOR);
@@ -27,20 +34,27 @@ public class ChordSpec {
 		this.quality = quality;
 		this.degree = degree;
 		this.inversion = inversion;
+		this.degreeQualityMap = new HashMap<>();
+		for (Degree adjusted : Degree.values())
+			degreeQualityMap.put(adjusted, Quality.MAJOR);
 	}
 	
 	// getters and setters:
-	public Note getTonic() { return tonic.clone(); }
-	public void setTonic(Note newTonic) { this.tonic = newTonic; } 
-	public Quality getQuality() { return quality; }
-	public void setQuality(Quality newQuality) { this.quality = newQuality; }
-	public Degree getDegree() { return degree; }
-	public void setDegree(Degree newDegree) { this.degree = newDegree; }
-	public void setAdd2(boolean add2OrNot) { this.add2 = add2OrNot; }
-	public void setAdd4(boolean add4OrNot) { this.add2 = add4OrNot; }
-	public void setAdd6(boolean add6OrNot) { this.add2 = add6OrNot; }
-	public int getInversion() { return inversion; }
-	public void setInversion(int newInversion) { this.inversion = newInversion; }
+	public Note 	getTonic() 						{ return tonic.clone(); }
+	public void 	setTonic(Note newTonic) 		{ this.tonic = newTonic; } 
+	public Quality 	getQuality() 					{ return quality; }
+	public void 	setQuality(Quality newQuality) 	{ this.quality = newQuality; }
+	public Degree 	getDegree() 					{ return degree; }
+	public void 	setDegree(Degree newDegree) 	{ this.degree = newDegree; }
+	public Quality 	getDegreeQuality(Degree degree) { return degreeQualityMap.get(degree); }
+	public void 	setDegreeQuality
+								(Degree degree, 
+								Quality quality) 	{ degreeQualityMap.put(degree, quality); }
+//	public void 	setAdd2(boolean add2OrNot) 		{ this.add2 = add2OrNot; }
+//	public void 	setAdd4(boolean add4OrNot) 		{ this.add2 = add4OrNot; }
+//	public void 	setAdd6(boolean add6OrNot) 		{ this.add2 = add6OrNot; }
+	public int 		getInversion() 					{ return inversion; }
+	public void 	setInversion(int newInversion) 	{ this.inversion = newInversion; }
 	
 	public Chord build() {
 		return builder().build();
@@ -65,16 +79,16 @@ public class ChordSpec {
 		}
 		switch(degree) {
 		case THIRTEENTH:
-			builder.addPitch(13);
+			builder.addPitch(13, degreeAdjustment(degreeQualityMap.get(THIRTEENTH)));
 			// fall through
 		case ELEVENTH:
-			builder.addPitch(11);
+			builder.addPitch(11, degreeAdjustment(degreeQualityMap.get(ELEVENTH)));
 			// fall through
 		case NINTH:
-			builder.addPitch(9);
+			builder.addPitch(9, degreeAdjustment(degreeQualityMap.get(NINTH)));
 			// fall through
 		case SEVENTH:
-			builder.addPitch(7);
+			builder.addPitch(7, degreeAdjustment(degreeQualityMap.get(SEVENTH)));
 			// fall through
 		default:
 		case NONE:
@@ -91,16 +105,42 @@ public class ChordSpec {
 		return builder;
 	}
 	
+	/**
+	 * e.g. "minor seventh" or "augmented eleventh" etc.
+	 * 
+	 * @param degreeQuality
+	 * @return accidental which maps to the given Quality, ignoring DIMINISHED
+	 */
+	private Accidental degreeAdjustment(Quality degreeQuality) {
+		return Accidental.values()[Math.max(degreeQuality.ordinal() - 1, 0)];
+	}
+	
 	@Override
 	public String toString() {
 		String name = "";
 		
 		name += tonic; // TODO Note lacks a proper toString for valid reasons
 		name += quality;
+		for (Degree interval : Degree.values()) {
+			if (interval == Degree.NONE)
+				continue;
+			if (interval.ordinal() <= degree.ordinal()) {
+				// calculate adjustment up to chord degree
+				Quality adjustment = degreeQualityMap.get(interval);
+				if (adjustment != Quality.MAJOR) {
+					// only list adjustment if it's not the default (MAJOR)
+					name += adjustment;
+					// list interval being adjusted, unless it's the chord's degree and is about to be listed
+					if (interval.ordinal() < degree.ordinal())
+						name += interval;
+				}
+			}
+		}
+		// list chord degree no matter what
 		name += degree;
-		name += add2 ? "add2" : "";
-		name += add4 ? "add4" : "";
-		name += add6 ? "add6" : "";
+//		name += add2 ? "add2" : "";
+//		name += add4 ? "add4" : "";
+//		name += add6 ? "add6" : "";
 		
 		// TODO handle inversion
 		
@@ -111,9 +151,9 @@ public class ChordSpec {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (add2 ? 1231 : 1237);
-		result = prime * result + (add4 ? 1231 : 1237);
-		result = prime * result + (add6 ? 1231 : 1237);
+//		result = prime * result + (add2 ? 1231 : 1237);
+//		result = prime * result + (add4 ? 1231 : 1237);
+//		result = prime * result + (add6 ? 1231 : 1237);
 		result = prime * result + ((degree == null) ? 0 : degree.hashCode());
 		result = prime * result + inversion;
 		result = prime * result + ((quality == null) ? 0 : quality.hashCode());
@@ -130,12 +170,12 @@ public class ChordSpec {
 		if (getClass() != obj.getClass())
 			return false;
 		ChordSpec other = (ChordSpec) obj;
-		if (add2 != other.add2)
-			return false;
-		if (add4 != other.add4)
-			return false;
-		if (add6 != other.add6)
-			return false;
+//		if (add2 != other.add2)
+//			return false;
+//		if (add4 != other.add4)
+//			return false;
+//		if (add6 != other.add6)
+//			return false;
 		if (degree != other.degree)
 			return false;
 		if (inversion != other.inversion)
@@ -152,7 +192,7 @@ public class ChordSpec {
 
 	public enum Quality {
 		
-		AUGMENTED("+"), MAJOR("M"), MINOR("m"), DIMINISHED("°");
+		DIMINISHED("°"), MINOR("m"), MAJOR("M"), AUGMENTED("+");
 		
 		private String notation;
 		
