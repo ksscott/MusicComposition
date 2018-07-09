85456a81498dddda8b9031df6915d21733414923
ksscott <scott.ksteven@gmail.com>
2018-07-09 15:01:51 -0600

    Attempt at Voice Timbre

--- MusicComposition/src/main/BeadRunner.java
+++ MusicComposition/src/main/BeadRunner.java
@@ -234,14 +234,58 @@ public class BeadRunner {
 				float value = 0;
 				for (int j=1; j<harmonics+1; j++) {
 					double harmonic = Math.sin(i*2*Math.PI/resolution*j);
-					double harmonicUpperNeighbor = Math.sin(i*2*Math.PI/resolution*(j*(1+neighborDist)));
-					double harmonicLowerNeighbor = Math.sin(i*2*Math.PI/resolution*(j*(1-neighborDist)));
+					double upperNeighborRatio = j*(1+neighborDist);
+					double lowerNeighborRatio = j*(1-neighborDist);
+					double harmonicUpperNeighbor = Math.sin(i*2*Math.PI/resolution*upperNeighborRatio);
+					double harmonicLowerNeighbor = Math.sin(i*2*Math.PI/resolution*lowerNeighborRatio);
 					double decayEnv = Math.pow(j, nToThe);
 					double sinusoidalEnv = rangeCenter+rangeHalf*Math.cos((j-1.2)*Math.PI/nthHarmonicIsMinimum);
 //					double sinusoidalEnv2 = Math.cos(rangeCenter+rangeHalf*Math.cos((j-1.2)*Math.PI/nthHarmonicIsMinimum/2.0));
 					double envFinal = decayEnv 
-									* sinusoidalEnv 
+							* sinusoidalEnv 
 //									* sinusoidalEnv2
+							;
+					value += harmonic * envFinal
+							+ harmonicUpperNeighbor * envFinal * neighborRatio
+							+ harmonicLowerNeighbor * envFinal * neighborRatio
+							;
+				}
+				buffer.buf[i] = value;
+			}
+		}
+		return new Timbre(120, buffer);
+	}
+	
+	public static Timbre getVoiceTimbre() {
+		int resolution = 10000;
+		int harmonics = Math.min(1000, resolution/4/20);
+		// decay envelope on harmonics:
+		double nToThe = -2.3;
+		// sinusoidal envelope on harmonics:
+		double nthHarmonicIsMinimum = 6;
+		double minimumHarmonicRatio = .6;
+		double rangeHalf = (1.0 - minimumHarmonicRatio)/2.0;
+		double rangeCenter = 1.0 - rangeHalf;
+		// add noisy neighbors:
+		double neighborDist = .07;
+		double neighborRatio = .7;
+		
+		Buffer buffer = new Buffer(resolution);
+		{
+			for (int i=0; i<resolution; i++) {
+				float value = 0;
+				for (int j=1; j<harmonics+1; j++) {
+					double harmonic = Math.sin(i*2*Math.PI/resolution*j);
+					double upperNeighborRatio = j*(1+neighborDist);
+					double lowerNeighborRatio = j*(1-neighborDist);
+					double harmonicUpperNeighbor = Math.sin(i*2*Math.PI/resolution*upperNeighborRatio);
+					double harmonicLowerNeighbor = Math.sin(i*2*Math.PI/resolution*lowerNeighborRatio);
+					double decayEnv = Math.pow(j, nToThe);
+					double sinusoidalEnv = rangeCenter+rangeHalf*Math.cos((j-1)*Math.PI/nthHarmonicIsMinimum);
+					double sinusoidalEnv2 = Math.cos(rangeCenter+rangeHalf*Math.cos((j-1)*Math.PI/nthHarmonicIsMinimum/3.0));
+					double envFinal = decayEnv 
+									* sinusoidalEnv 
+									* sinusoidalEnv2
 									;
 					value += harmonic * envFinal
 						   + harmonicUpperNeighbor * envFinal * neighborRatio
@@ -251,7 +295,7 @@ public class BeadRunner {
 				buffer.buf[i] = value;
 			}
 		}
-		return new Timbre(120, buffer);
+		return new Timbre(200, buffer);
 	}
 
 	public static float random(double x) {
