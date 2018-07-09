60b58b1fdc0680ef25472267d058f5c95696411d
ksscott <scott.ksteven@gmail.com>
2018-07-09 15:04:15 -0600

    Polyphonic strategy. It will happen one day, I believe

---
+++ MusicComposition/src/composing/strategy/PolyphonicProgressionStrategy.java
@@ -0,0 +1,24 @@
+package composing.strategy;
+
+import composing.IncompleteComposition;
+import theory.Measure;
+
+public class PolyphonicProgressionStrategy implements ComposingStrategy {
+
+	@Override
+	public Measure generateFirstMeasure() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean iterate(IncompleteComposition composition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "Polyphonic Progression";
+	}
+}
